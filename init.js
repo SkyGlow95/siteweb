d5bca01fcf351ff6d7bab97b96d3853da26779ae
SkyGlow95 <119793126+SkyGlow95@users.noreply.github.com>
2022-12-06 12:09:26 +0100

    Add files via upload

--- init.js
+++ init.js
@@ -1,3 +1,4 @@
+/*--Apparition au scroll--*/
 const ratio = .5
 const options = {
 	root: null,
@@ -19,49 +20,53 @@ document.querySelectorAll('.reveal').forEach(function(r) {
 	observer.observe(r)
 })
 
-
+/*--Menu--*/
 const menuHamburger = document.querySelector(".menuh")
 const navLinks = document.querySelector(".navMenu")
+const nav = document.querySelector(".nav")
 const menua = document.querySelector(".navMenu")
 
 menuHamburger.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
 })
 
+menuHamburger.addEventListener('click',()=>{
+nav.classList.toggle('mobile-menu')
+})
+
 menua.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
 })
 
+menua.addEventListener('click',()=>{
+nav.classList.toggle('mobile-menu')
+})
 
-var bttn = document.getElementById('bttn')
-var bttnr = document.getElementById('bttnr')
-var bttns = document.getElementById('bttns')
+/*--Expériences--*/
+const bttn = document.getElementById('bttn')
+const bttnr = document.getElementById('bttnr')
+const bttns = document.getElementById('bttns')
 function Click() {
 	bttnr.style.backgroundColor = 'white'
 	bttns.style.backgroundColor = 'white'
-	bttn.style.backgroundColor = 'blue'
+	bttn.style.backgroundColor = '#008CBA'
 }
-var bttn = document.getElementById('bttn')
-var bttnr = document.getElementById('bttnr')
-var bttns = document.getElementById('bttns')
 function leftClick() {
 	bttn.style.backgroundColor = 'white'
 	bttns.style.backgroundColor = 'white'
-	bttnr.style.backgroundColor = 'blue'
+	bttnr.style.backgroundColor = '#008CBA'
 }
-var bttn = document.getElementById('bttn')
-var bttnr = document.getElementById('bttnr')
-var bttns = document.getElementById('bttns')
 function rightClick() {
 	bttnr.style.backgroundColor = 'white'
 	bttn.style.backgroundColor = 'white'
-	bttns.style.backgroundColor = 'blue'
+	bttns.style.backgroundColor = '#008CBA'
 }
-$(document).ready(function() {
-	$("#bttn").click(function() {
-	$("#bdrr").hide();
+/*--$(allias de jQuery)--*/
+$(document).ready(function() { /*--créer une fonction pour la page--*/
+	$("#bttn").click(function() { /*--lors du clique sur l'élément--*/
+	$("#bdrr").hide(); /*--cacher l'élément--*/
 	$("#bdrs").hide();
-	$("#bdr").show();
+	$("#bdr").show(); /*--montrer l'élément--*/
 	});
 	$("#bttnr").click(function() {
 	$("#bdr").hide();
@@ -74,9 +79,13 @@ $(document).ready(function() {
 	$("#bdrs").show();
 	});
 	});
+window.addEventListener("load", () => { /*--quand la page load--*/
+	document.getElementById("bdrr").style.display = "none"; /*--récupérer l'élément et changer sont style--*/
+	document.getElementById("bdrs").style.display = "none";
+});
 
-
-$(".slider").owlCarousel({
+/*--Compétences--*/
+$(".slider").owlCarousel({ /*--appel de l'élément slider pour définir des attributs--*/
 	loop: true,
 	autoplay: true,
 	autoplayTimeout: 2000, //2000ms = 2s;
